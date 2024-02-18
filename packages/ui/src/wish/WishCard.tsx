72633be08fbe773c1442a5db164091d164d3f90f
HAMEL Julien <zweir.dd@gmail.com>
2024-02-18 22:30:55 +0100

    refactor: fix typecheck error

--- packages/ui/src/wish/WishCard.tsx
+++ packages/ui/src/wish/WishCard.tsx
@@ -10,7 +10,7 @@ import {
 } from "@nextui-org/react"
 import { SquarePenIcon, Trash2Icon } from "lucide-react"
 
-import type { Wish } from "@my-wishlist/types/wish"
+import type { Wish } from "@my-wishlist/types/Wish"
 
 type Props = {
   wish: Wish
