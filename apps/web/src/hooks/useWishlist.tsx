052ef0a43bc8603cb1c179830e5b98c0327aceae
HAMEL Julien <zweir.dd@gmail.com>
2024-02-21 10:35:06 +0100

    refactor: clear wishlist on sign out

--- apps/web/src/hooks/useWishlist.tsx
+++ apps/web/src/hooks/useWishlist.tsx
@@ -3,17 +3,25 @@ import { useEffect } from "react"
 
 import { getWishes } from "@my-wishlist/api/routes/wish"
 
+import useSession from "@/hooks/useSession"
 import useWishlistStore from "@/stores/wishlist"
 
 const useWishlist = () => {
+  const { session } = useSession()
   const { setWishlist, wishlist, ...wishlistStore } = useWishlistStore()
   const { data } = useFetch(getWishes, { disabled: wishlist.length > 0 })
 
   useEffect(() => {
+    if (!session) {
+      setWishlist([])
+
+      return
+    }
+
     if (data?.result) {
       setWishlist(data?.result || [])
     }
-  }, [data?.result, setWishlist])
+  }, [data?.result, setWishlist, session])
 
   return { setWishlist, wishlist, ...wishlistStore }
 }
