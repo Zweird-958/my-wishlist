7daf55710986833f9f0d7ef746151479b4deee5f
HAMEL Julien <zweir.dd@gmail.com>
2024-07-02 21:51:54 +0200

    refactor: remove unnecessary files

--- apps/desktop/src/utils/config.ts
+++ apps/desktop/src/utils/config.ts
@@ -3,14 +3,12 @@ import { z } from "zod"
 const schema = z.object({
   store: z.object({
     sessionKey: z.string(),
-    localeKey: z.string(),
   }),
 })
 
 const config = schema.parse({
   store: {
     sessionKey: "my-wishlist-session",
-    localeKey: "my-wishlist-locale",
   },
 })
 
--- apps/web/src/components/ui/Center.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react"
-
-const Center = ({ children }: { children: ReactNode }) => (
-  <div className="flex items-center justify-center grow">{children}</div>
-)
-export default Center
