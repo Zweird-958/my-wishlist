bb2f6c7be502b02907e313cefc73c001c4c2337f
HAMEL Julien <zweir.dd@gmail.com>
2024-07-23 18:33:19 +0200

    refactor: remove unnecessary comment

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -241,8 +241,6 @@ export default tseslint.config(
           allowConstantLoopConditions: true,
         },
       ],
-      // "@typescript-eslint/no-non-null-assertion": "error",
-      // "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
     },
   },
   {
