5d8d2ccc26e0613ace8bed8a3e2297c4e537c75b
HAMEL Julien <zweir.dd@gmail.com>
2024-07-31 10:50:11 +0200

    chore(next): add transpilePackages + strict mode

--- apps/desktop/next.config.mjs
+++ apps/desktop/next.config.mjs
@@ -3,7 +3,14 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  transpilePackages: ["@my-wishlist/ui"],
+  transpilePackages: [
+    "@my-wishlist/ui",
+    "@my-wishlist/config",
+    "@my-wishlist/db",
+    "@my-wishlist/i18n",
+    "@my-wishlist/schemas",
+    "@my-wishlist/types",
+  ],
 }
 
 export default nextConfig
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: [
+    "@my-wishlist/ui",
+    "@my-wishlist/config",
+    "@my-wishlist/db",
+    "@my-wishlist/i18n",
+    "@my-wishlist/schemas",
+    "@my-wishlist/types",
+  ],
+}
 
 export default nextConfig
