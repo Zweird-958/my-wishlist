dc3822094d85f7070b3d0c0e6e7e21e674b6326a
zweird <zweir.dd@gmail.com>
2024-10-24 21:15:01 +0200

    fix(image): blur don't work on android

--- apps/mobile/src/components/wishlist/wish-image.tsx
+++ apps/mobile/src/components/wishlist/wish-image.tsx
@@ -1,6 +1,6 @@
 import { BlurView } from "expo-blur"
-import { Image, type ImageProps } from "expo-image"
-import { View } from "react-native"
+import { Image, ImageBackground, type ImageProps } from "expo-image"
+import { Platform, View } from "react-native"
 
 import { useTheme } from "@/components/contexts/ThemeContext"
 
@@ -8,6 +8,23 @@ type Props = { image: ImageProps["source"] }
 
 const WishImage = ({ image }: Props) => {
   const { tw } = useTheme()
+
+  if (Platform.OS === "android") {
+    return (
+      <ImageBackground
+        source={image}
+        style={tw.style("w-full h-full")}
+        imageStyle={tw.style("rounded-md")}
+        blurRadius={5}
+      >
+        <Image
+          source={image}
+          contentFit="contain"
+          style={tw.style("w-full h-full rounded-md")}
+        />
+      </ImageBackground>
+    )
+  }
 
   return (
     <>
