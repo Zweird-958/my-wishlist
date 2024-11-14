423cbf11c2cedf13202c4c54bdaf754204d9f010
zweird <83603824+Zweird-958@users.noreply.github.com>
2024-11-14 15:55:15 +0100

    fix(types): some files needs tanstack types

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,4 +1,9 @@
-export * from "./src/hooks/use-mutation"
-export * from "./src/hooks/use-query"
+export * from "@tanstack/react-query"
 
-export { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+export { useMutation, useGenericMutation } from "./src/hooks/use-mutation"
+export { useQuery, useGenericQuery } from "./src/hooks/use-query"
+
+// eslint-disable-next-line no-duplicate-imports
+export type { MutationOptions } from "./src/hooks/use-mutation"
+// eslint-disable-next-line no-duplicate-imports
+export type { QueryOptions } from "./src/hooks/use-query"
