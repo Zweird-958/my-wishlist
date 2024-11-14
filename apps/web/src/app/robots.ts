188731aa5e241d053ec6f84a6d18ed86ce8d8e91
zweird <83603824+Zweird-958@users.noreply.github.com>
2024-11-14 12:50:17 +0100

    refactor(api): use vercel env + get api preview url

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next"
 
 const robots = (): MetadataRoute.Robots => {
   const isProduction =
-    (process.env.NEXT_PUBLIC_APP_ENV ?? process.env.NODE_ENV) === "production"
+    (process.env.NEXT_PUBLIC_APP_ENV ?? process.env.VERCEL_ENV) === "production"
 
   if (!isProduction) {
     return {
--- packages/utils/src/config.ts
+++ packages/utils/src/config.ts
@@ -4,8 +4,16 @@ const schema = z.object({
   apiUrl: z.string(),
 })
 
+const getApiUrl = () => {
+  if (process.env.VERCEL_ENV === "preview") {
+    return `${process.env.NEXT_PUBLIC_PROTOCOL}${process.env.VERCEL_URL}${process.env.NEXT_PUBLIC_API_PATH}`
+  }
+
+  return process.env.NEXT_PUBLIC_API_URL ?? process.env.EXPO_PUBLIC_API_URL
+}
+
 const config = schema.parse({
-  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? process.env.EXPO_PUBLIC_API_URL,
+  apiUrl: getApiUrl(),
 })
 
 export default config
