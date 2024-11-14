0c2dbc32fca459e4635e4bc7921499f9264d7f60
zweird <83603824+Zweird-958@users.noreply.github.com>
2024-11-14 13:22:32 +0100

    fix(api): cant read vercel env variables

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -2,7 +2,8 @@ import type { MetadataRoute } from "next"
 
 const robots = (): MetadataRoute.Robots => {
   const isProduction =
-    (process.env.NEXT_PUBLIC_APP_ENV ?? process.env.VERCEL_ENV) === "production"
+    (process.env.NEXT_PUBLIC_APP_ENV ?? process.env.NEXT_PUBLIC_VERCEL_ENV) ===
+    "production"
 
   if (!isProduction) {
     return {
--- packages/utils/src/config.ts
+++ packages/utils/src/config.ts
@@ -5,8 +5,8 @@ const schema = z.object({
 })
 
 const getApiUrl = () => {
-  if (process.env.VERCEL_ENV === "preview") {
-    return `${process.env.NEXT_PUBLIC_PROTOCOL}${process.env.VERCEL_URL}${process.env.NEXT_PUBLIC_API_PATH}`
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+    return `${process.env.NEXT_PUBLIC_PROTOCOL}${process.env.NEXT_PUBLIC_VERCEL_URL}${process.env.NEXT_PUBLIC_API_PATH}`
   }
 
   return process.env.NEXT_PUBLIC_API_URL ?? process.env.EXPO_PUBLIC_API_URL
