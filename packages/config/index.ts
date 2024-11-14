5a71a27c195720976b858f7b377233345080e220
zweird <83603824+Zweird-958@users.noreply.github.com>
2024-11-14 09:22:42 +0100

    refactor(config): use currency schema for default currency

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -4,7 +4,7 @@ export const currencies = ["DOLLAR", "EURO", "POUND"] as const
 export const currencySchema = z.enum(currencies)
 
 const schema = z.object({
-  defaultCurrency: z.enum(currencies),
+  defaultCurrency: currencySchema,
   defaultFilter: z.enum(["all", "private", "notPrivate"]),
   defaultSort: z.enum(["date", "priceAsc", "priceDesc"]),
 })
