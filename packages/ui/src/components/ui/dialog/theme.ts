7a6224e009fb7a946bab3eced51db8a2ca5b591c
zweird <zweir.dd@gmail.com>
2024-11-29 09:49:03 +0100

    refactor(dialog): remove border and ring

--- packages/ui/src/components/ui/dialog/theme.ts
+++ packages/ui/src/components/ui/dialog/theme.ts
@@ -4,10 +4,14 @@ export const dialogVariants = tv({
   slots: {
     overlay:
       "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80",
-    content:
-      "bg-content data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] rounded-lg border shadow-lg duration-200",
+    content: [
+      "bg-content text-content-foreground",
+      "flex w-full max-w-lg flex-col rounded-lg shadow-lg",
+      "fixed left-[50%] top-[50%] z-50 translate-x-[-50%] translate-y-[-50%] duration-200",
+      "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+    ],
     close:
-      "ring-offset-card focus:ring-ring data-[state=open]:bg-content data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none",
+      "data-[state=open]:bg-content data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none",
     header: "flex flex-col py-3 text-center sm:text-left",
     footer: "flex flex-row justify-between gap-3 py-3",
     title: "text-lg font-semibold leading-none tracking-tight",
